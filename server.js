5bb6f4bc7f169b29da853039be59d21fe8f6edad
henperi <henry.izontimi@gmail.com>
2018-08-06 05:49:07 -0700

    Removing session store

--- server.js
+++ server.js
@@ -61,9 +61,9 @@ app.use(session({
     secret: 'some_random_generated_const_string',
     resave: true,
     saveUninitialized: true,
-    store: sessionStore.createSessionStore({
-        type: 'mongodb',
-    })
+    // store: sessionStore.createSessionStore({
+    //     type: 'mongodb',
+    // })
 }));
 
 //Express Flash and Messaging Middleware
